1ad5b90dc390cd9eabec80ba004adeebe60d6929
Jaime Carvajal <jaacarvajal27@gmail.com>
2018-09-28 23:06:10 +0800

    Updated the website with the of inclusion of Unsplash api

---
+++ webapp/js/main.js
@@ -0,0 +1,15 @@
+// $(document).ready(function() {
+//   $.getJSON("https://api.unsplash.com/photos/?client_id=9f775bdbac9e08ea9395a8b666b7e9e9d55826827f20cf119a6e8ce5ef1c7925", function(data) {
+//     console.log(data);
+//     var i = 0;
+//     $.each(data, function(index, value) {
+//       var user = value.user.username;
+//       var likes = value.likes;
+//       var photo = value.urls.regular;
+//       // console.log(".title." + i);
+//       $(".name." + i).text(user);
+//       // $(".title." + i).text(likes);
+//       i++;
+//     });
+//   });
+// });
