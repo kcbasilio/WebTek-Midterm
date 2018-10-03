5e8793fdb68183d27b9a5f314f78dc6e4686a26f
Jaime Carvajal <jaacarvajal27@gmail.com>
2018-10-03 13:12:28 +0800

    Updated web app design, added react web app

--- webapp/js/main.js
+++ webapp/js/main.js
@@ -1,3 +1,4 @@
+// LIST NEW PHOTOS
 // $(document).ready(function() {
 //   $.getJSON("https://api.unsplash.com/photos/?client_id=9f775bdbac9e08ea9395a8b666b7e9e9d55826827f20cf119a6e8ce5ef1c7925", function(data) {
 //     console.log(data);
@@ -8,8 +9,23 @@
 //       var photo = value.urls.regular;
 //       // console.log(".title." + i);
 //       $(".name." + i).text(user);
+//       $(".image." + i).attr("src", photo);
 //       // $(".title." + i).text(likes);
 //       i++;
 //     });
 //   });
+// });
+
+// LIST COLLECTIONS 
+// $(document).ready(function() {
+//     $.getJSON("https://api.unsplash.com/collections/featured?client_id=9f775bdbac9e08ea9395a8b666b7e9e9d55826827f20cf119a6e8ce5ef1c7925", function(data) {
+//       console.log(data);
+//     });
+// });
+
+// // SEARCH PHOTO BY KEYWORD
+// $(document).ready(function() {
+//     $.getJSON("https://api.unsplash.com/search/photos/?page=2&query=forest&client_id=9f775bdbac9e08ea9395a8b666b7e9e9d55826827f20cf119a6e8ce5ef1c7925", function(data) {
+//       console.log(data);
+//     });
 // });
