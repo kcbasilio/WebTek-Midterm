44d2a91a797568a6e12b64808cdf0cd258772c90
Jaime Carvajal <jaacarvajal27@gmail.com>
2018-11-12 20:22:20 +0800

    Updated template, added search function using ingredients or nutrients.

--- webapp/js/main.js
+++ webapp/js/main.js
@@ -1,88 +1,208 @@
 $(document).ready(function() {
-	var domain = "https://api.edamam.com/search"; // domain
-	var app_id = "40cae619"; // application id (developer.edamam.com) 
-	var app_key = "1b6db03230c404ca39f1ea74de1724fc"; // application key (developer.edamam.com) 
-	$("#submit-recipe").click( function() {
-		let query = $("#recipe").val(); // get input of the user
-		$.getJSON(domain + "?q=" + query + "&app_id=" + app_id + "&app_key=" + app_key, function(data) {
-			let counter = 1;
-			let allRecipes = [];
-			$.each(data, function() {			
-				$.each(data.hits, function(index, value) {
-					let recipe = {
-						label: value.recipe.label,
-						image: value.recipe.image,
-						calories: value.recipe.calories,
-						ingredients: value.recipe.ingredientLines,
-						dietLabels: value.recipe.dietLabels,
-						healthLabels: value.recipe.healthLabels,
-						nutrients: {
-							energy: value.recipe.totalNutrients.ENERC_KCAL,
-							fat: value.recipe.totalNutrients.FAT,
-							chocdf: value.recipe.totalNutrients.CHOCDF,
-							sugar: value.recipe.totalNutrients.SUGAR,
-							protein: value.recipe.totalNutrients.PROCNT,
-							cholesterol: value.recipe.totalNutrients.CHOLE,
-							sodium: value.recipe.totalNutrients.NA,
-							calcium: value.recipe.totalNutrients.CA,
-							magnesium: value.recipe.totalNutrients.MG,
-							potassium: value.recipe.totalNutrients.K,
-							iron: value.recipe.totalNutrients.FE,
-							zinc: value.recipe.totalNutrients.ZN,
-							phosphorus: value.recipe.totalNutrients.P,
-							vitA: value.recipe.totalNutrients.VITA_RAE,
-							vitb6: value.recipe.totalNutrients.VITB6A,
-							vitD: value.recipe.totalNutrients.VITD,
-							vitE: value.recipe.totalNutrients.TOCPHA,
-							vitK: value.recipe.totalNutrients.VITK1
-						}
 
-					}
-					allRecipes[counter] = recipe;
+	var app = {}; // app object
+	const APP_KEY = "bxIsZkPLBemsheW6F9cMwxaufuttp1HSkPqjsnixH8AgtcIbW5"; // application key from https://market.mashape.com/Dauntless27/applications/WikiCook
 
+	/*
+	Search Recipes By Ingredients
+	*/
+	app.searchRecipesByIngredients = function() {
+
+		let querySearch = $("#search-ingredient").val(); // ingredient/s query
+		// let api_endpoint = `https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/findByIngredients?ingredients=${querySearch}&fillIngredients=true&number=10`; // api endpoint with parameters
+		$.ajax({
+			url: '../json/searchbyingredients.json',
+			// headers: {
+			// 	"X-Mashape-Key": APP_KEY,
+			// 	"Accept": "application/json"
+			// },
+			success: function (data) {
+				let i = 1;
+				let counter = 0;
+				$.each(data, (index, value) => {
+					$("#info-title" + i).text(value.title);
+					$("#image-container" + i).attr("src", value.image);
+					app.getRecipeInformation(value.id, counter);
+					app.createResultSubBox(value.title, value.image, counter+1);
 					counter++;
+					i++;
+				})
+		  },
+			error: function (data) {
+		  	console.log(data);
+		  }
+		})
+
+	}
+
+	/*
+	Search Recipes By Nutrients(Calorie, Protein, Fat, Carbo)
+	*/
+	app.searchRecipeByNutrients = function() {
+
+		let maxCalorie = $('#search-max-calorie').val();
+		let minCalorie = $('#search-min-calorie').val();
+		let maxProtein = $('#search-max-protein').val();
+		let minProtein = $('#search-min-protein').val();
+		let maxFat = $('#search-max-fat').val();
+		let minFat = $('#search-min-fat').val();
+		let maxCarb = $('#search-max-carbs').val();
+		let minCarb = $('#search-min-carbs').val();
+		// let api_endpoint = `https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/findByNutrients?maxCalories=${maxCalorie}&maxCarbs=${maxCarb}&maxFat=${maxFat}&maxProtein=${maxProtein}&minCalories=${maxCarb}&minCarbs=${minCarb}&minFat=${minFat}&minProtein=${minProtein}`;  // api endpoint with parameters
+		$.ajax({
+			url: '../json/searchbynutrients.json',
+			// headers: {
+			// 	"X-Mashape-Key": APP_KEY,
+			// 	"Accept": "application/json"
+			// },
+			success: function (data) {
+				let i = 1;
+				let counter = 0;
+				$.each(data, (index, value) => {
+					$("#info-title" + i).text(value.title);
+					$("#image-container" + i).attr("src", value.image);
+					app.getRecipeInformation(value.id, counter);
+					app.createResultSubBox(value.title, value.image, counter+1);
+					counter++;
+					i++;
+				})
+		  },
+			error: function (data) {
+		  	console.log(data);
+		  }
+		})
+
+	}
+
+	/*
+	Get Recipe Informations
+	*/
+	app.getRecipeInformation = function(id, counter) {
+
+		// let api_endpoint = `https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/${id}/information?includeNutrition=true`;
+		let allIngredients = [];
+		$.ajax({
+			url: `../json/${id}.json`,
+			// headers: {
+			// 	"X-Mashape-Key": APP_KEY,
+			// 	"Accept": "application/json"
+			// },
+			success: function (data) {
+				let c = 0;
+				$.each(data, () => {
+					app.displayIngredients(data, counter);
+					app.displayNutritionFacts(data, counter);
+					return false;
 				});
-				return false;
-			});
-			for (let i in allRecipes) {
-				$("#recipe-title" + i).text(allRecipes[i].label);
-				$("#info-title" + i).text(allRecipes[i].label);
-				$("#result-subbox-image" + i).attr("src", allRecipes[i].image);
-				$("#image-container" + i).attr("src", allRecipes[i].image);
-			}
-			// nutrition facts
-			for (let i in allRecipes) {
-				$("#energy" + i).text(allRecipes[i].nutrients.energy.quantity.toFixed(2) + " " + allRecipes[i].nutrients.energy.unit);
-				$("#fat" + i).text(allRecipes[i].nutrients.fat.quantity.toFixed(2) + " " + allRecipes[i].nutrients.fat.unit);
-				$("#chocdf" + i).text(allRecipes[i].nutrients.chocdf.quantity.toFixed(2) + " " + allRecipes[i].nutrients.chocdf.unit);
-				$("#sugar" + i).text(allRecipes[i].nutrients.sugar.quantity.toFixed(2) + " " + allRecipes[i].nutrients.sugar.unit);
-				$("#protein" + i).text(allRecipes[i].nutrients.protein.quantity.toFixed(2) + " " + allRecipes[i].nutrients.protein.unit);
-				$("#cholesterol" + i).text(allRecipes[i].nutrients.cholesterol.quantity.toFixed(2) + " " + allRecipes[i].nutrients.cholesterol.unit);
-				$("#sodium" + i).text(allRecipes[i].nutrients.sodium.quantity.toFixed(2) + " " + allRecipes[i].nutrients.sodium.unit);
-				$("#calcium" + i).text(allRecipes[i].nutrients.calcium.quantity.toFixed(2) + " " + allRecipes[i].nutrients.calcium.unit);
-				$("#magnesium" + i).text(allRecipes[i].nutrients.magnesium.quantity.toFixed(2) + " " + allRecipes[i].nutrients.magnesium.unit);
-				$("#potassium" + i).text(allRecipes[i].nutrients.potassium.quantity.toFixed(2) + " " + allRecipes[i].nutrients.potassium.unit);
-				$("#iron" + i).text(allRecipes[i].nutrients.iron.quantity.toFixed(2) + " " + allRecipes[i].nutrients.iron.unit);
-				$("#zinc" + i).text(allRecipes[i].nutrients.zinc.quantity.toFixed(2) + " " + allRecipes[i].nutrients.zinc.unit);
-				$("#phosphorus" + i).text(allRecipes[i].nutrients.phosphorus.quantity.toFixed(2) + " " + allRecipes[i].nutrients.phosphorus.unit);
-				$("#vitA" + i).text(allRecipes[i].nutrients.vitA.quantity.toFixed(2) + " " + allRecipes[i].nutrients.vitA.unit);
-				$("#vitb6" + i).text(allRecipes[i].nutrients.vitb6.quantity.toFixed(2) + " " + allRecipes[i].nutrients.vitb6.unit);
-				$("#vitD" + i).text(allRecipes[i].nutrients.vitD.quantity.toFixed(2) + " " + allRecipes[i].nutrients.vitD.unit);
-				$("#vitE" + i).text(allRecipes[i].nutrients.vitE.quantity.toFixed(2) + " " + allRecipes[i].nutrients.vitE.unit);
-				$("#vitK" + i).text(allRecipes[i].nutrients.vitK.quantity.toFixed(2) + " " + allRecipes[i].nutrients.vitK.unit);
-			}
-  		});
-  	});
+		  },
+			error: function (data) {
+		  	console.log(data);
+		  }
+		})
+
+	}
+
+	/*
+	Classify Cuisine of recipe
+	*/
+	app.classifyCuisineOfRecipe = function (ingredientList, title) {
+
+		// let api_endpoint = `https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/${id}/information?includeNutrition`;
+		//
+		// $.ajax({
+		// 	type: 'POST',
+		// 	url: api_endpoint,
+		// 	headers: {
+		// 		"X-Mashape-Key": app_key,
+		// 		"Accept": "application/json"
+		// 	},
+		// 	data: {
+		// 		'ingredientList': ingredientList,
+		// 		'title': title
+		// 	}
+		// 	success: function(data) {
+		//     console.log(data);
+		//   },
+		// 	error: function(data) {
+		//   	console.log(data);
+		//   }
+		// })
+
+	}
+
+	/*
+	Display Recipe Ingredients
+	*/
+	app.displayIngredients = function (ingredients, counter) {
+
+		let i = 0;
+		let recipe_ingredient = []; // array for ingredients per recipe
+
+		$.each(ingredients.extendedIngredients, (index, value) => {
+			recipe_ingredient[i] = ingredients.extendedIngredients;
+			i++;
+			return false;
+		}); // put ingredients per recipe(array) in the array(nested array)
+
+		for (let x = 0; x < recipe_ingredient[0].length; x++) {
+				let li = document.createElement("li"); // create <li> element
+				li.textContent = recipe_ingredient[0][x].originalString; // set the ingredient as text of the created element
+				li.setAttribute('class', 'list-group-item'); // set class attribute value to add style
+				document.getElementsByClassName('list-group')[counter].appendChild(li); // append element to the ul
+		}
+
+	}
+
+	/*
+	Display Recipe Nutrition
+	*/
+	app.displayNutritionFacts = function (ingredients, counter) {
+
+		let i = 0;
+		let recipe_nutrition = []; // array for nutritions per recipe
+
+		$.each(ingredients.nutrition.nutrients, (index, value) => {
+			recipe_nutrition[i] = ingredients.nutrition.nutrients;
+			i++;
+			return false;
+		}); // put nutrition per recipe(array) in the array(nested array)
+
+		for (let x = 0; x < recipe_nutrition[0].length; x++) {
+			let tr = document.createElement('tr');
+			let tdLabel = document.createElement('td');
+			let tdQuantity = document.createElement('td');
+			tdLabel.textContent = recipe_nutrition[0][x].title;
+			tdQuantity.textContent = recipe_nutrition[0][x].amount + " " + recipe_nutrition[0][x].unit;
+			tr.appendChild(tdLabel);
+			tr.appendChild(tdQuantity);
+			document.getElementsByTagName('tbody')[counter].appendChild(tr);
+		}
+	}
+
+	app.createResultSubBox = function (title, image, counter) {
+
+		let dataNumber = ["one","two","three","four","five","six","seven","eight","nine","ten"];
+
+		let figure = document.createElement('figure');
+		let image_div = document.createElement('div');
+		let title_div = document.createElement('div');
+		let h1 = document.createElement('h1');
+		figure.setAttribute("class", "result-subbox");
+		figure.setAttribute("data-number", dataNumber[counter-1]);
+		image_div.setAttribute("class", "image-section");
+		image_div.setAttribute("style", "background-image: url(" + image + ")");
+		title_div.setAttribute("class", "title-section");
+		h1.setAttribute("class", "lead display-4");
+		h1.setAttribute("id", "recipe-title" + counter);
+		h1.textContent = title;
+		title_div.appendChild(h1);
+		figure.appendChild(image_div);
+		figure.appendChild(title_div);
+		document.getElementsByClassName("result-box")[0].appendChild(figure);
+
+	}
+
+	app.start = document.getElementById('search-recipe-ingredients').addEventListener("click", app.searchRecipesByIngredients);
+	app.start = document.getElementById('search-recipe-nutritions').addEventListener("click", app.searchRecipeByNutrients);
+
 });
-
-// $(document).ready(function() {
-//     $.getJSON("", function(data) {
-//      	console.log(data);
-//     });
-// });
-
-// $(document).ready(function() {
-//     $.getJSON("", function(data) {
-//      	console.log(data);
-//     });
-// });
